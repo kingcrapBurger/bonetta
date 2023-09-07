0d7aac95a0318d24083be18afcb829d849a0fa40
kingcrapBurger <jangpegasus@naver.com>
2023-09-07 13:21:29 +0900

    230907 자바스크립트 로딩순서 정리 완료 모바일 오류 없듬

--- src/main/resources/static/js/treat.js
+++ src/main/resources/static/js/treat.js
@@ -3,57 +3,53 @@ let sec1 = document.getElementById("sec1");
 let MainImageHeight;
 let c_width;
 
-// onload
+// onload 실행시
 window.onload = function () {
   deviceWidth();
   changeImg(c_width);
-  MainImageHeight = getParentHeight();
-  adjustParentHeight(MainImageHeight);
-  if (c_width < 601) {
-    sec1resize(MainImageHeight);
-  }
-  // headerScroll();
-
 };
-// resize
+
+// resize 실행시
 window.addEventListener("resize", function () {
   deviceWidth();
   changeImg(c_width);
-  MainImageHeight = getParentHeight();
-  adjustParentHeight(MainImageHeight);
-  if (c_width < 601) {
-    sec1resize(MainImageHeight);
-  }
-
 });
-// 모바일화면 사진 교체
-function changeImg(element) {
-  backgroundImage = document.getElementById("background_image");
-  if (element < 601) {
-    backgroundImage.src = "/images/2.treat/P2_1_mobile.jpg";
-  } else {
-    backgroundImage.src = "/images/2.treat/P2_1.png";
-  }
-}
-//scroll
-// window.addEventListener("scroll", headerScroll);
-
-// 모바일 sec1 디자인 변경
-function sec1resize(element) {
-  sec1.style.height = `${element + 150}px`;
-}
 
 // 디바이스 너비 구하기
 function deviceWidth() {
   c_width = document.documentElement.clientWidth;
 }
+
+// 모바일화면 사진 교체
+function changeImg(element) {
+  let backgroundImage = document.getElementById("background_image");
+  let newSrc;
+  if (element < 601) {
+    newSrc = "/images/2.treat/P2_1_mobile.jpg";
+  } else {
+    newSrc = "/images/2.treat/P2_1.png";
+  }
+  backgroundImage.src = newSrc;
+
+  // 메인화면사진 로딩 완료후 실행
+  backgroundImage.onload = function () {
+    MainImageHeight = getParentHeight();
+    adjustParentHeight(MainImageHeight);
+    if (c_width < 601) {
+      sec1resize(MainImageHeight);
+    }
+  }
+}
+
 // 부모 요소의 높이 구하기
 function getParentHeight() {
-  backgroundImage = document.getElementById("background_image");
+  let backgroundImage = document.getElementById("background_image");
   let imageHeight = backgroundImage.height;
+  // console.log("imageHeight 는 = ", imageHeight);
   return imageHeight;
   // sec1.style.height = `${imageHeight}px`;
 }
+
 // 부모 요소의 높이 조정
 function adjustParentHeight(element) {
   sec1.style.height = `${element}px`;
@@ -61,28 +57,11 @@ function adjustParentHeight(element) {
   clickBox.style.height = `${element}px`;
 }
 
-// function headerScroll() {
-//   let scrollTop = window.scrollY;
-//   let winBottom = scrollTop + window.innerHeight;
+// 모바일 sec1 디자인 변경
+function sec1resize(element) {
+  sec1.style.height = `${element + 150}px`;
+}
 
-//   let actReady1Elements = document.querySelectorAll(".fadeReady1");
-//   let actReady2Elements = document.querySelectorAll(".fadeReady2");
-//   actReady1Elements.forEach((element) => {
-//     let location = element.offsetTop;
-//     console.log("location :" + location);
-//     console.log("winBottom :" + winBottom);
-//     if (winBottom - 30 > location) {
-//       element.classList.add("fadeIn1");
-//       // element.classList.remove("fadeReady1");
-//     }
-//   });
-//   actReady2Elements.forEach((element) => {
-//     let location = element.offsetTop;
-//     if (winBottom - 30 > location) {
-//       element.classList.add("fadeIn2");
-//       // element.classList.remove("fadeReady2");
-//     }
-//   });
 
 // body2 슬라이드 처리
 let underLine = Array.from(document.querySelectorAll(".underLine"));
@@ -97,17 +76,17 @@ underLine.forEach((element, index) => {
     if (element.classList.contains("enlarge")) {
       return false;
     } else {
-      for (var i=0; i < underLine.length; i++) {
+      for (var i = 0; i < underLine.length; i++) {
         underLine[i].classList.remove("enlarge");
       }
       element.classList.add("enlarge");
-      
+
       //슬라이드 애니메이션
       // var eleIndex = getIndex(element);
       let slideWidth = slide.clientWidth;
-      console.log("slideWidth : "+ slideWidth);
+      console.log("slideWidth : " + slideWidth);
       slidesContainer.style.left = ("-" + slideWidth * index) + "px";
-      console.log("slidesContainer.style.left : "+ slidesContainer.style.left);
+      console.log("slidesContainer.style.left : " + slidesContainer.style.left);
 
       // querySelector(".slideContainer").sytle.left = eleIndex * silde.Width;
 
@@ -170,44 +149,51 @@ function smallBtn(element) {
   }
 }
 
-// carousel item 처리
+
+// carousel 이벤트
 let carouselList = document.querySelector(".carousel__list");
 let carouselItems = document.querySelectorAll(".carousel__item");
 let elems = Array.from(carouselItems);
 
-carouselList.addEventListener("click", function (event) {
-  var newActive = event.target.closest(".carousel__item");
-  console.log(newActive);
-  var isItem = newActive.closest(".carousel__item");
-  console.log("isItem :", isItem);
-  update(newActive);
+// carouselList.addEventListener("click", function (event) {
+//   var newActive = event.target.closest(".carousel__item");
+//   console.log(newActive);
+//   update(newActive);
+// });
+elems.forEach(element =>{
+  element.addEventListener("click", function(event){
+    console.log("event.target : ",this);
+    update(this)
+  });
 });
 
 let update = function (newActive) {
-  let newActivePos = newActive.dataset.pos;
-  console.log("newActivePos :", newActivePos);
+  let newItemPos = newActive.dataset.pos;
+  console.log("newItemPos :", newItemPos);
 
   let current = elems.find((elem) => elem.dataset.pos == 0);
   let prev = elems.find((elem) => elem.dataset.pos == -1);
   let next = elems.find((elem) => elem.dataset.pos == 1);
 
   [current, prev, next].forEach((item) => {
-    var itemPos = item.dataset.pos;
+    var oldItemPos = item.dataset.pos;
 
-    item.dataset.pos = getPos(itemPos, newActivePos);
+    item.dataset.pos = getPos(oldItemPos, newItemPos);
   });
 };
 
-let getPos = function (current, active) {
-  let diff = current - active;
+let getPos = function (current, selected) {
+  let diff = current - selected;
   console.log("diff :", diff);
 
-  if (Math.abs(current - active) > 1) {
+  if (Math.abs(current - selected) > 1) {
     return -current;
   }
 
   return diff;
-};
+};  // // carousel 이벤트 end
+
+
 
 
 
